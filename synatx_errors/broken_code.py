2cab865f4b5ef59ba3fba884031dbb89052eb244
anshkunj <meghag2075@gmail.com>
2026-02-08 18:08:36 +0530

    Create broken_code.py

---
+++ synatx_errors/broken_code.py
@@ -0,0 +1,15 @@
+# Broken Python code with syntax errors
+
+def greet(name)
+    print("Hello, " + name)
+
+for i in range(5)
+    print(i)
+
+if True
+print("This will cause an indentation error")
+
+x = 5
+y = 10
+if x < y
+    print("x is less than y")
