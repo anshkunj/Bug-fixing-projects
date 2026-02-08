abfac8e76cd3383e464c91ee3b5762175cc98ccd
anshkunj <meghag2075@gmail.com>
2026-02-08 18:09:05 +0530

    Create fixed_code.py

---
+++ synatx_errors/fixed_code.py
@@ -0,0 +1,15 @@
+# Fixed Python code
+
+def greet(name):
+    print("Hello, " + name)
+
+for i in range(5):
+    print(i)
+
+if True:
+    print("This will run correctly")
+
+x = 5
+y = 10
+if x < y:
+    print("x is less than y")
